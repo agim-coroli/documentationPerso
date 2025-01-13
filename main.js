abded66a96b753c17d74f1885f6f3d077a07a7af
Agim Coroli <agim.coroli8016@gmail.com>
2025-01-13 23:16:28 +0100

    v3

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-function normalize(str){
-    return str.replace('-','/')
-}
-console.log(
-    normalize('20-05-2017'));
+function add(s) {
+    let summand1 = parseInt(s, 10);
+    let indexPlus = s.indexOf('+');
+    let sAfterPlus = s.substr(indexPlus + 1);
+    let summand2 = parseInt(sAfterPlus, 10);
+    return summand1 + summand2;
+  }
+
+  console.log(add("10"));
+  
